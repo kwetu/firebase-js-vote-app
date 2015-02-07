1ba65bdb8cb0a22a87b0fc03d5fc21187a07dcff
root <Nik>
2015-02-07 14:44:42 +0200

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# firebase-js-vote-app
+# Simple Ionic Voting App. Firebase in server side.
+
+1. cordova build --release android
+2. keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 1000
+3. jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore AGTPlusApp-release-unsigned.apk  alias_name
+4. /home/nik/Android/Sdk/build-tools/21.1.2/zipalign -v 4 AGTPlusApp-release-unsigned.apk   AGTPlus.apk
--- www/js/vote.js
+++ www/js/vote.js
@@ -1,21 +1,25 @@
 $(document).ready(function () {
 
-        var fb = new Firebase("https://vote-app.firebaseio.com/");
+        var fb = new Firebase("https://vote-app.firebaseio.com/Голосование"); //firebase connect
+        var msg = new Firebase("https://vote-app.firebaseio.com/Отзывы"); //firebase connect
+
 
         // monitors changes and updates UI
-        fb.child('counter1').on('value', updateDiv);
-        fb.child('counter2').on('value', updateDiv);
-        fb.child('counter3').on('value', updateDiv);
+        //fb.child('counter1').on('value', updateDiv);
+        //fb.child('counter2').on('value', updateDiv);
+        //fb.child('counter3').on('value', updateDiv);
+        //
+        //fb.on('value', updatePre);
 
-        fb.on('value', updatePre);
-
-        // creates a new, incremental record
+        // creates a new, incremental records
         $('#inc1').on('click', incId1);
         $('#inc2').on('click', incId2);
         $('#inc3').on('click', incId3);
+        $('#submit').on('click', submitMsg);
+
 
         function blockButton() {
-            //Block button and Set timeout
+            //Block buttons and Set timeout
             $('#inc1').prop("disabled", true);
             $('#inc2').prop("disabled", true);
             $('#inc3').prop("disabled", true);
@@ -25,18 +29,8 @@ $(document).ready(function () {
                 $('#inc2').prop('disabled', false);
                 $('#inc3').prop('disabled', false);
 
-            }, 5000);
+            }, 5000); //Set vote timeout
         }
-
-        // resets the data
-        $('#clear').on('click', function () {
-            fb.remove();
-        });
-
-        // attempts to create any id you put in
-        $('#customButton').on('click', function () {
-            addRecord($('#custom').val());
-        });
 
         var errId = 0;
         // creates a new, incremental record
@@ -46,7 +40,7 @@ $(document).ready(function () {
             swal("Спасибо за оценку!", "Ваше мнение важно для нас", "success");
 
             // increment the counter
-            fb.child('counter1').transaction(function (currentValue) {
+            fb.child('Хорошо').transaction(function (currentValue) {
                 return (currentValue || 0) + 1
             }, function (err, committed, ss) {
                 if (err) {
@@ -59,7 +53,7 @@ $(document).ready(function () {
                 }
             });
 
-            blockButton();
+            blockButton(); //Block buttons to vote
 
 
         }
@@ -70,7 +64,7 @@ $(document).ready(function () {
             //Sweet Alert
             swal("Спасибо за оценку!", "Ваше мнение важно для нас", "success");
             // increment the counter
-            fb.child('counter2').transaction(function (currentValue) {
+            fb.child('Нормально').transaction(function (currentValue) {
                 return (currentValue || 0) + 1
             }, function (err, committed, ss) {
                 if (err) {
@@ -95,7 +89,7 @@ $(document).ready(function () {
 
 
             // increment the counter
-            fb.child('counter3').transaction(function (currentValue) {
+            fb.child('Плохо').transaction(function (currentValue) {
                 return (currentValue || 0) + 1
             }, function (err, committed, ss) {
                 if (err) {
@@ -109,28 +103,52 @@ $(document).ready(function () {
             });
             blockButton();
 
+        }
 
+        //add new msg to firebase
+        function submitMsg(err, committed) {
+            if (err) { //check errors
+                console.log('msg submiting error');
+                $('#messageInput').val('');
+                $('#nameInput').val('');
+            }
+            else if (committed) { //submit data
+                var name = $('#nameInput').val();
+                var text = $('#messageInput').val();
+                msg.push({name:name, text:text});
+                $('#messageInput').val('');
+                $('#nameInput').val('');
 
+                swal("Спасибо за отзыв!", "Ваше мнение важно для нас", "success"); //sweet alert
+            }
 
         }
 
-        // creates new incremental record
-        function addRecord(id) {
-            setTimeout(function () {
-                fb.child('records').child('rec' + id).set('record #' + id, function (err) {
-                    err && setError(err);
-                });
-            });
+        //add new msg to firebase
+        function submitMsg() {
+
+                var name = $('#nameInput').val();
+                var text = $('#messageInput').val();
+                msg.push({name:name, text:text});
+                $('#messageInput').val('');
+                $('#nameInput').val('');
+
+                swal("Спасибо за отзыв!", "Ваше мнение важно для нас", "success"); //sweet alert
+                $('#exampleModal').modal('hide'); //close modal
+
         }
 
-        function updateDiv(ss) {
-            $('#show').text(ss.val() || 0);
-            $('#custom').val('rec' + (parseInt(ss.val(), 10) + 1));
-        }
 
-        function updatePre(ss) {
-            $('#data').text(JSON.stringify(ss.val(), null, 2));
-        }
+
+
+
+
+        // Add a callback that is triggered for each chat message.
+        msg.on('child_added', function (snapshot) {
+            var message = snapshot.val();
+            $('<div/>').text(message.text).prepend($('<em/>').text(message.name+': ')).appendTo($('#messagesDiv'));
+            $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+        });
 
         function setError(msg) {
             var id = ++errId;
