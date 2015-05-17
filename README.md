eb25f03496c7d9398ef68411052bc1a11d9c9210
Николай Сутковой <n.sutkovoy@gmail.com>
2015-05-17 12:56:29 +0300

    update proj

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Simple Ionic Voting App. Firebase in server side.
 
-Release notes:
-*v.0.2.1 -  add user feedback*
 
-
-# How to build
+## How to build
 1. Generate a release build for Android `cordova build --release android`
-2. Generate our private key using the keytool `keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 1000`
-3. Sign the unsigned APK, run the jarsigner tool `jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore AGTPlusApp-release-unsigned.apk  alias_name`
-4. This signs the apk in place. Finally, we need to run the zip align tool to optimize the APK: `/home/nik/Android/Sdk/build-tools/21.1.2/zipalign -v 4 AGTPlusApp-release-unsigned.apk   AGTPlus.apk`
+2. CD in folder /data/www/agt-js-vote/platforms/android/ant-build
+3. Generate our private key using the keytool `о пот`
+4. Sign the unsigned APK, run the jarsigner tool `jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore AGTPlusApp-release-unsigned.apk  alias_name`
+5. This signs the apk in place. Finally, we need to run the zip align tool to optimize the APK: `/home/nik/Android/Sdk/build-tools/21.1.2/zipalign -v 4 AGTPlusApp-release-unsigned.apk   AGTPlus.apk`
+6. `cp /data/www/agt-js-vote/platforms/android/ant-build/AGTPlus.apk /data/www/agt-js-vote/platforms/android/build/`
--- www/js/vote.js
+++ www/js/vote.js
@@ -2,29 +2,27 @@ $(document).ready(function () {
 
         var fb = new Firebase('https://vote-app.firebaseio.com/'), //firebase connect
             golos = fb.child('Голосование'),
-                gd = golos.child('Хорошо'),
-                nm = golos.child('Нормально'),
-                bd = golos.child('Плохо');
-        var msg = new Firebase("https://vote-app.firebaseio.com/Отзывы"); //firebase feedback connect
+            gd = golos.child('Хорошо'),
+            nm = golos.child('Нормально'),
+            bd = golos.child('Плохо'),
+            msg = fb.child("Отзывы"); //firebase feedback connect
 
 
         // creates a new, incremental records
         $('#inc1').on('click', incId1);
         $('#inc2').on('click', incId2);
         $('#inc3').on('click', incId3);
+
+        //onclick submit func
         $('#submit').on('click', submitMsg);
 
 
         function blockButton() {
             //Block buttons and Set timeout
-            $('#inc1').prop("disabled", true);
-            $('#inc2').prop("disabled", true);
-            $('#inc3').prop("disabled", true);
+            $('#inc1, #inc2, #inc3').prop("disabled", true);
 
             setTimeout(function () {
-                $('#inc1').prop('disabled', false);
-                $('#inc2').prop('disabled', false);
-                $('#inc3').prop('disabled', false);
+                $('#inc1, #inc2, #inc3').prop('disabled', false);
 
             }, 5000); //Set vote timeout
         }
@@ -32,9 +30,6 @@ $(document).ready(function () {
         var errId = 0;
         // creates a new, incremental record
         function incId1() {
-
-            //Sweet Alert
-            swal("Спасибо за оценку!", "Ваше мнение важно для нас", "success");
 
             // increment the counter
             gd.child('Голосов').transaction(function (currentValue) {
@@ -50,18 +45,18 @@ $(document).ready(function () {
                 }
             });
 
-            blockButton(); //Block buttons to vote
+            //Sweet Alert
+            swal("Дякуємо за оцінку!", "Ваша думка важлива для нас", "success");
 
+            blockButton(); //Block buttons to vote
 
         }
 
         //second
 
         function incId2() {
-            //Sweet Alert
-            swal("Спасибо за оценку!", "Ваше мнение важно для нас", "success");
-            // increment the counter
 
+            // increment the counter
             nm.child('Голосов').transaction(function (currentValue) {
                 return (currentValue || 0) + 1;
             }, function (err, committed, ss) {
@@ -75,16 +70,17 @@ $(document).ready(function () {
                 }
             });
 
+            //Sweet Alert
+            swal("Дякуємо за оцінку!", "Ваша думка важлива для нас", "success");
+
             blockButton();
         }
 
         //third
 
         function incId3() {
-            //Sweet Alert
-            swal("Спасибо за оценку!", "Ваше мнение важно для нас", "success");
 
-
+            // increment the counter
             bd.child('Голосов').transaction(function (currentValue) {
                 return (currentValue || 0) + 1;
             }, function (err, committed, ss) {
@@ -98,6 +94,9 @@ $(document).ready(function () {
                 }
             });
 
+            //Sweet Alert
+            swal("Дякуємо за оцінку!", "Ваша думка важлива для нас", "success");
+
             blockButton();
 
             //experimental
@@ -108,60 +107,30 @@ $(document).ready(function () {
         }
 
 
-
-
-        ////add new msg to firebase
-        //function submitMsg(err, committed) {
-        //    if (err) { //check errors
-        //        console.log('msg submiting error');
-        //        $('#messageInput').val('');
-        //        $('#nameInput').val('');
-        //    }
-        //    else if (committed) { //submit data
-        //        var name = $('#nameInput').val();
-        //        var text = $('#messageInput').val();
-        //        msg.push({name:name, text:text});
-        //        $('#messageInput').val('');
-        //        $('#nameInput').val('');
-        //
-        //        swal("Спасибо за отзыв!", "Ваше мнение важно для нас", "success"); //sweet alert
-        //    }
-        //
-        //}
-
         //add new msg to firebase
         function submitMsg() {
 
             var name = $('#nameInput').val();
             var text = $('#messageInput').val();
 
-            if (name && text !== "") //check if not empty
-            {
+            if (name && text !== "") {
                 msg.push({
-                            name: name,
-                            text: text,
-                            timestamp: Firebase.ServerValue.TIMESTAMP
-                         });
+                    name: name,
+                    text: text,
+                    timestamp: Firebase.ServerValue.TIMESTAMP
+                });
                 $('#messageInput').val('');
                 $('#nameInput').val('');
 
-                swal("Спасибо за отзыв!", "Ваше мнение важно для нас", "success"); //sweet alert
+                swal("Дякуємо за відгук!", "Ваша думка важлива для нас", "success");
                 $('#exampleModal').modal('hide'); //close modal
             }
 
             else {
-                alert("Enter name and msg!");
+                alert("Enter name and message!");
             }
 
         }
-
-
-        // Add a callback that is triggered for each chat message.
-        //msg.on('child_added', function (snapshot) {
-        //    var message = snapshot.val();
-        //    $('<div/>').text(message.text).prepend($('<em/>').text(message.name+': ')).appendTo($('#messagesDiv'));
-        //    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-        //});
 
         function setError(msg) {
             var id = ++errId;
